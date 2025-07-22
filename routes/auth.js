82606d1e9779793c99175f7ab6244e9d6af6964d
Leilasempere <leilasempere@gmail.com>
2025-07-22 11:52:01 +0200

    lol

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { register, login, verifyEmail, requestPasswordReset, resetPassword } from '../controllers/authController.js';
+import { authorize, protect } from '../middlewares/authMiddleware.js';
+import { validate } from '../middlewares/validationMiddleware.js';
+import { createUserSchema } from '../validations/userValidation.js';
+import { createUser } from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/verify/:token', verifyEmail);
+
+
+
+router.post('/password-reset-request', requestPasswordReset);
+router.post('/reset-password/:token', resetPassword);
+router.post('/', validate(createUserSchema), createUser);
+
+
+router.get('/user-profile', protect, (req, res) => {
+  res.json({ message: `Bienvenue ${req.user.name}` });
+});
+
+router.get('/admin-panel', protect, authorize('admin'), (req, res) => {
+  res.json({ message: 'Bienvenue dans le panneau admin' });
+ 
+});
+
+router.get('/moderator-section', protect, authorize('moderator', 'admin'), (req, res) => {
+  res.json({ message: 'Bienvenue dans la section modérateur' });
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+import deleteUserCron from "./middlewares/deleteUser.js";
+import auditDependencies from './middlewares/auditDependencies.js'
+import cors from 'cors';
+import helmet from 'helmet';    
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors()); 
+app.use(helmet()); // Sécurise les en-têtes HTTP
+
+
+// Démarre le cron
+deleteUserCron.start();
+auditDependencies.start()
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+  server.close(() => process.exit(1));
+});
